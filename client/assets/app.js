5c13aace83b92dc8b5f709fe612247fa11091e55
Benjamin Huang <benjaminthuang@gmail.com>
2017-05-08 02:31:59 -0700

    footer

--- client/assets/app.js
+++ client/assets/app.js
@@ -3,14 +3,28 @@ var app = angular.module('app', ["ui.router", "ngStorage"]);
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
   .state('home', { 
-    url: '/', templateUrl: 'partials/home.html'
-    // , 
-    // controller: 'homeCtrl' 
+    url: '/', templateUrl: 'partials/home.html'    
   })
-  .state('register', { 
-    url: '/register', templateUrl: 'partials/register.html'
-    // , 
-    // controller: 'registerCtrl' 
+  .state('blog', { 
+    url: '/blog', templateUrl: 'partials/blog.html'    
+  })
+  .state('contact', { 
+    url: '/contact', templateUrl: 'partials/contact.html'    
+  })
+  .state('donate', { 
+    url: '/donate', templateUrl: 'partials/donate.html'    
+  })
+  .state('events', { 
+    url: '/events', templateUrl: 'partials/events.html'    
+  })
+  .state('join', { 
+    url: '/join', templateUrl: 'partials/join.html'    
+  })
+  .state('post', { 
+    url: '/post', templateUrl: 'partials/post.html'    
+  })
+  .state('sign_in', { 
+    url: '/sign_in', templateUrl: 'partials/sign_in.html'
   })
 
 
